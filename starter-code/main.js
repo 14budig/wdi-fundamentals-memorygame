70dc9fcc4124516f34df4aca4d6cf3be34f2d0db
14budig <nbudig@gmail.com>
2016-08-14 06:43:57 -0700

    Add files via upload
    Final version for real.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,3 +1,4 @@
+<<<<<<< 4f2ca8bafc1dd2a89e951ce8790c2703a3a68ed2
 /*var cardOne = "King";
 var cardTwo = "King";
 var cardThree = "Queen";
@@ -55,3 +56,62 @@ function isMatch(cards) {
 }
 
 createBoard();
+=======
+/*var cardOne = "King";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "Queen";
+
+if(cardOne == cardTwo){
+	alert("yes");
+}*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+
+}
+
+function isTwoCards() {
+  // add card to array of cards being viewed
+  // 'this' is not covered in the pre-work but
+  // for now, just know it gives you access to the cardElement you click on
+  cardsInPlay.push(this.getAttribute('data-card'));
+	// show the card's image
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+	} else {
+		this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
+	}
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+    // clear cards in play array for next try
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  // alert winning message
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
+>>>>>>> a900de6da4a66e95ee67a8641d7bea0e037bff44
